a71f40ae4ac7a589a6849c4aaa3755d89cda1c78
pliniocefet <plinio.cefet@gmail.com>
2020-09-03 08:23:30 -0300

    Acrescentado a pasta template e alterações no arquivo app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route('/index')
 def index():
 	return render_template()
+
+
 
 if __name__ == "__main__"
